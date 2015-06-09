0b8771e261aade57838a3650cbcd0a592ed3923d
cuihw <cui-hw@hotmail.com>
2015-06-09 23:52:25 +0800

    compose the main activity

--- com.szch/src/com/bluetooth/connect/BlueToothConnect.java
+++ com.szch/src/com/bluetooth/connect/BlueToothConnect.java
@@ -6,7 +6,6 @@ import java.util.Date;
 import java.util.Set;
 import java.util.UUID;
 
-
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothServerSocket;
@@ -18,26 +17,28 @@ import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
 
-public class BlueToothConnect{
+public class BlueToothConnect {
 
     public static BlueToothConnect mInstence;
-    
+
     private Context mContext;
-    
-    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+    private BluetoothAdapter bluetoothAdapter = BluetoothAdapter
+            .getDefaultAdapter();
 
     private static Handler mHandler;
 
-    private static final UUID MY_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private static final UUID MY_UUID = UUID
+            .fromString("00001101-0000-1000-8000-00805F9B34FB");
 
     private static final String NAME_SECURE = "name_secure";
 
     public static final int NO_PAIRED_DEVICES = 1;
 
     protected static final String TAG = null;
-    
+
     BluetoothServerSocket mBluetoothServerSocket;
-    
+
     private void init() {
         if (!bluetoothAdapter.isEnabled()) {
             bluetoothAdapter.enable();
@@ -46,29 +47,29 @@ public class BlueToothConnect{
         if (bluetoothAdapter.isDiscovering()) {
             bluetoothAdapter.cancelDiscovery();
         }
-        
+
         Set<BluetoothDevice> pairedList = bluetoothAdapter.getBondedDevices();
 
         if (pairedList.size() == 0) {
             Message msg = mHandler.obtainMessage(NO_PAIRED_DEVICES);
             mHandler.sendMessage(msg);
         }
-        
-        connectDevices();
 
+        connectDevices();
 
     }
 
     private void connectDevices() {
         try {
-            mBluetoothServerSocket = bluetoothAdapter.listenUsingRfcommWithServiceRecord(NAME_SECURE, MY_UUID);
-            
+            mBluetoothServerSocket = bluetoothAdapter
+                    .listenUsingRfcommWithServiceRecord(NAME_SECURE, MY_UUID);
+
             BluetoothSocket socket = mBluetoothServerSocket.accept();
-            
+
             if (socket != null) {
-                startReadingThread(socket);                
+                startReadingThread(socket);
             }
-            
+
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
@@ -76,15 +77,15 @@ public class BlueToothConnect{
     }
 
     private boolean stop = false;
-    
+
     private void startReadingThread(final BluetoothSocket socket) {
 
-        new Thread (new Runnable () {
+        new Thread(new Runnable() {
 
             @Override
             public void run() {
                 InputStream is = null;
-                
+
                 try {
                     is = socket.getInputStream();
                     while (true) {
@@ -92,7 +93,7 @@ public class BlueToothConnect{
                             break;
                         }
                         byte[] buffer = new byte[256];
-                        
+
                         int length = is.read();
 
                         if (length > 0) {
@@ -101,20 +102,18 @@ public class BlueToothConnect{
                         }
                     }
 
-
                 } catch (IOException e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
                 }
-                
+
             }
-            
+
         }).start();
-        
-        
+
     }
 
-    public static BlueToothConnect getInstence (Context c, Handler h) {
+    public static BlueToothConnect getInstence(Context c, Handler h) {
         if (mInstence == null) {
             mInstence = new BlueToothConnect(c, h);
         }
@@ -122,10 +121,10 @@ public class BlueToothConnect{
         return mInstence;
     }
 
-    private BlueToothConnect (Context c, Handler h) {
+    private BlueToothConnect(Context c, Handler h) {
         mContext = c;
         mHandler = h;
-        
+
         new Thread(new Runnable() {
 
             @Override
@@ -133,34 +132,38 @@ public class BlueToothConnect{
 
                 init();
 
-            }}).start();
+            }
+        }).start();
     }
 
-    //广播接收器
+    // 广播接收器
     private BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
         @Override
         public void onReceive(Context context, Intent intent) {
             String action = intent.getAction();
 
             if (BluetoothTools.ACTION_NOT_FOUND_SERVER.equals(action)) {
-                //未发现设备
-                //serversText.append("not found device\r\n");
-                
-            } else if (BluetoothTools.ACTION_FOUND_DEVICE.equals(action)) {
-                //获取到设备对象
-                BluetoothDevice device = (BluetoothDevice)intent.getExtras().get(BluetoothTools.DEVICE);
-                //deviceList.add(device);
-                //serversText.append(device.getName() + "\r\n");
-                
-            } else if (BluetoothTools.ACTION_CONNECT_SUCCESS.equals(action)) {
-                //连接成功
-                
-            } else if (BluetoothTools.ACTION_DATA_TO_GAME.equals(action)) {
-                //接收数据
-                TransmitBean data = (TransmitBean)intent.getExtras().getSerializable(BluetoothTools.DATA);
-                String msg = "from remote " + new Date().toLocaleString() + " :\r\n" + data.getMsg() + "\r\n";
+                // 未发现设备
+                // serversText.append("not found device\r\n");
 
-            } 
+            } else if (BluetoothTools.ACTION_FOUND_DEVICE.equals(action)) {
+                // 获取到设备对象
+                BluetoothDevice device = (BluetoothDevice) intent.getExtras()
+                        .get(BluetoothTools.DEVICE);
+                // deviceList.add(device);
+                // serversText.append(device.getName() + "\r\n");
+
+            } else if (BluetoothTools.ACTION_CONNECT_SUCCESS.equals(action)) {
+                // 连接成功
+
+            } else if (BluetoothTools.ACTION_DATA_TO_GAME.equals(action)) {
+                // 接收数据
+                TransmitBean data = (TransmitBean) intent.getExtras()
+                        .getSerializable(BluetoothTools.DATA);
+                String msg = "from remote " + new Date().toLocaleString()
+                        + " :\r\n" + data.getMsg() + "\r\n";
+
+            }
         }
-     };
+    };
 }
--- com.szch/src/com/szch/MainActivity.java
+++ com.szch/src/com/szch/MainActivity.java
@@ -1,22 +1,38 @@
 package com.szch;
 
+import java.text.SimpleDateFormat;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+    
+    private EditText mEditText_date;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
+        setContentView(R.layout.scrollview);
+        
+        initView();
     }
 
+    private void initView() {
+        SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String date = sDateFormat.format(new java.util.Date());
 
+        mEditText_date = (EditText)findViewById(R.id.edittext_date);
+        mEditText_date.setText(date);
+    }
 
+//    @Override
+//    public boolean onCreateOptionsMenu(Menu menu) {
+//        // Inflate the menu; this adds items to the action bar if it is present.
+//        getMenuInflater().inflate(R.menu.main, menu);
+//        return true;
+//    }
+    
+    
 }
